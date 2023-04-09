69f831ea185586af95493e4c81f2ac84980458a7
tzutingspace <tzutingh2@gmail.com>
2023-04-10 00:53:19 +0800

    [Modify] databases method

--- lambda/layer/python/database.py
+++ lambda/layer/python/database.py
@@ -16,6 +16,48 @@ class Database:
     def __init__(self):
         self.connection = pymysql.connect(**db_settings)
 
+    def queryall(self, sql, params=None):
+        with self.connection.cursor() as cursor:
+            try:
+                if params:
+                    cursor.execute(sql, params)
+                else:
+                    cursor.execute(sql)
+                result = [dict((cursor.description[i][0], value)
+                               for i, value in enumerate(row)) for row in cursor.fetchall()]
+            except:
+                result = 'ERROR'
+        return result
+
+    def insert(self, sql, params=None):
+        with self.connection.cursor() as cursor:
+            try:
+                if params:
+                    cursor.execute(sql, params)
+                else:
+                    cursor.execute(sql)
+                self.connection.commit()
+                result = cursor.lastrowid
+            except:
+                result = 'ERROR'
+            return result
+
+    def update(self, sql, params=None, many=None):
+        with self.connection.cursor() as cursor:
+            try:
+                if params:
+                    if many:
+                        cursor.executemany(sql, params)
+                    else:
+                        cursor.execute(sql, params)
+                else:
+                    cursor.execute(sql)
+                result = cursor.rowcount
+                self.connection.commit()
+            except:
+                result = 'ERROR'
+        return result
+
     def close(self):
         self.connection.close()
 
