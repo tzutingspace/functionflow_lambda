1aaaff763eb292cb0979589d449cb9f69d22e89b
tzutingspace <tzutingh2@gmail.com>
2023-04-19 15:05:59 +0800

    [Modify] classify obj

--- lambda/code/scheduler/scheduler.py
+++ lambda/code/scheduler/scheduler.py
@@ -2,102 +2,80 @@ import os
 from datetime import datetime, timedelta, timezone
 
 from database import Database
-from put_to_sqs import put_to_sqs
+from job_handler import Job
+from job_handler import Workflow
+from job_handler import QueueObj
 
 
 def lambda_handler(event, context):
+    # 抓取本次Scheduler區間
     dt0 = datetime.utcnow().replace(tzinfo=timezone.utc)
     current_date_and_time = dt0.astimezone(timezone(timedelta(hours=8)))
     time_interval = timedelta(minutes=int(os.environ["MIN_TIME_INTERVAL"]))
     end_date_and_time = current_date_and_time + time_interval
 
-    print(f"本次預計抓取區間 {current_date_and_time} ~ {end_date_and_time}")
+    print(f"本次Scheuler預計抓取區間 {current_date_and_time} ~ {end_date_and_time}")
 
-    # 寫到 queue和  workflows_instances
-    connection = Database().connection
+    db = Database()
 
-    with connection.cursor() as cursor:
-        # 找出本次符合條件的 workflow
-        sql = 'SELECT id, name, trigger_type, next_execute_time FROM workflows \
-            WHERE status = "active" \
-                AND next_execute_time >= %(current_date_and_time)s \
-                    AND next_execute_time <= %(end_date_and_time)s'
-        cursor.execute(
-            sql,
-            {
-                "current_date_and_time": current_date_and_time,
-                "end_date_and_time": end_date_and_time,
-            },
-        )
-        # 取得所有資料
-        work_flows = [
-            dict((cursor.description[i][0], value) for i, value in enumerate(row))
-            for row in cursor.fetchall()
-        ]
+    sql = """SELECT
+            id,
+            name,
+            trigger_type,
+            next_execute_time,
+            schedule_interval,
+            trigger_interval_seconds
+            FROM workflows
+            WHERE
+                status = "active"
+                AND trigger_type = "scheduled"
+                AND next_execute_time >= %(current_date_and_time)s
+                AND next_execute_time <= %(end_date_and_time)s
+            """
+    params = {
+        "current_date_and_time": current_date_and_time,
+        "end_date_and_time": end_date_and_time,
+    }
 
-        print(f"本次預計跑的 workflow 有 {len(work_flows)} 筆")
+    workflows = db.queryall(sql, params)
+    print(f"本次預計跑的 workflow 有 {len(workflows)} 筆")
 
-    for work_flow in work_flows:
-        QUEUE_OBJ = {}
-        with connection.cursor() as cursor:
-            # Init workflow instances info
-            work_flow["next_execute_time"] = work_flow["next_execute_time"].strftime(
-                "%Y-%m-%d %H:%M:%S"
+    for workflow in workflows:
+        queue_obj = QueueObj("jobsQueue", {})
+        # init workflow instance
+        workflow_obj = Workflow(workflow)
+        workflow_obj.calculate_next_execute_time()
+        workflow_obj.update_db_workflow_next_execute_time(db)
+        workflow_obj.create_wf_instance(db)
+
+        # 將資訊放進Queue_obj中
+        queue_obj.init_workflow(vars(workflow_obj))
+
+        # 取得該筆 workflow 所有的jobs
+        jobs_detail = workflow_obj.get_jobs(db)
+        prev_job_instance_id = None
+
+        print(f">>該 workflow 有 {len(jobs_detail)} 筆Job")
+        # create job instances sequentially
+        for job_detail in jobs_detail:
+            job = Job(job_detail)
+
+            # 第一個JOB
+            if job.sequence == 1:
+                queue_obj.change_step(vars(job))
+
+            # 建立job instance
+            prev_job_instance_id = job.create_job_instance(
+                workflow_obj.wf_instance_id, prev_job_instance_id, db
             )
-            work_flow["status"] = "queued"
-            work_flow["execution_time"] = current_date_and_time.strftime("%Y-%m-%d %H:%M:%S")
 
-            # 建立 workflows_instances
-            sql = 'INSERT INTO workflows_instances\
-                    (workflows_id, `status`, trigger_type, execution_time, external_trigger)\
-                        VALUES(%(id)s, %(status)s, %(trigger_type)s, %(execution_time)s, "f")'
-            cursor.execute(sql, work_flow)
-            work_flow["wf_instance_id"] = cursor.lastrowid
-            print(f">>建立work_flow_instances {work_flow}")
-            connection.commit()
+            # 將資料寫到queue_obj
+            print(f">>>> 建立job_instances{vars(job)}")
+            queue_obj.add_step(vars(job))
 
-            # ## 取得該筆 workflow 所有的jobs
-            sql = "SELECT jobs.id, jobs.name, jobs.sequence, jobs.depends_job_id,\
-                  jobs.config_input, jobs.config_output, `functions`.name as function_name,\
-                  `functions`.id as function_id \
-                FROM `jobs` INNER JOIN `functions` ON jobs.function_id = `functions`.id \
-                    WHERE workflow_id = %(id)s ORDER BY sequence"
-            cursor.execute(sql, work_flow)
+        # 將此queueObj 丟到SQS
+        queue_obj.put_to_sqs()
 
-            job_details = [
-                dict((cursor.description[i][0], value) for i, value in enumerate(row))
-                for row in cursor.fetchall()
-            ]
-            QUEUE_OBJ["workflow"] = work_flow
-            print(f">>該 workflow 有 {len(job_details)} 筆Job")
-        with connection.cursor() as cursor:
-            QUEUE_OBJ["steps"] = {}
-            QUEUE_OBJ["ready_execute_job"] = []
-            job_instance_id = None
-            # 建立 jobs_instances
-            for job in job_details:
-                print(f">>>> 建立job_instances{job}")
-                job["status"] = "waiting"
-                job["depends_job_instance_id"] = job_instance_id
-                job["start_time"] = None
-                job["end_time"] = None
-                job["result_output"] = None
-                QUEUE_OBJ["ready_execute_job"].append(job["name"])
-                if job["sequence"] == 1:
-                    QUEUE_OBJ["step_now"] = job["name"]
-                sql = f"INSERT INTO jobs_instances (workflow_instance_id, name, status, \
-                    sequence, config_input, config_output, depends_job_instance_id) \
-                    VALUES ({work_flow['wf_instance_id']}, %(name)s, %(status)s,  %(sequence)s, \
-                    %(config_input)s, %(config_output)s, %(depends_job_instance_id)s)"
-                cursor.execute(sql, job)
-                job_instance_id = cursor.lastrowid
-                job["id"] = job_instance_id
-                job_name = job["name"]
-                QUEUE_OBJ["steps"][job_name] = job
-            connection.commit()
-        # 加入queue中
-        put_to_sqs(QUEUE_OBJ, "jobsQueue")
-        print(f'workflows_instances_id={work_flow["wf_instance_id"]}, 已加入SQS')
+    db.close()
 
-    connection.close()
-    return {"msg": "Sceduler Success"}
+    return {"lambda msg": "Sceduler Success"}
--- lambda/layer/python/job_handler.py
+++ lambda/layer/python/job_handler.py
@@ -1,37 +1,18 @@
-from datetime import datetime, timezone, timedelta
 import json
+import os
 import re
+from datetime import datetime, timedelta, timezone
+
+import boto3
+from botocore.exceptions import ClientError
+from dateutil.relativedelta import relativedelta
 from error_handler import MyErrorHandler
 
-# 每個lambda都可以共用
 
-
+# Function
 def get_now_time():
     dt0 = datetime.utcnow().replace(tzinfo=timezone.utc)
     return dt0.astimezone(timezone(timedelta(hours=8))).strftime("%Y-%m-%d %H:%M:%S")
-
-
-def get_job_info_and_init(body):
-    job_name = body["step_now"]
-    job_info = body["steps"][job_name]
-    job_info["start_time"] = get_now_time()
-    return job_info
-
-
-def update_job_info(job_info, results_output):
-    job_info["end_time"] = get_now_time()
-    job_info["result_output"] = {}
-    config_outputs = parseString(job_info["config_output"])
-    for output in config_outputs:
-        out_put_name = output["name"]
-        job_info["result_output"][out_put_name] = results_output[out_put_name]
-    return job_info
-
-
-def update_body(body, job_info):
-    job_name = body["step_now"]
-    body["steps"][job_name] = job_info
-    return body
 
 
 def parseString(json_string):
@@ -41,6 +22,28 @@ def parseString(json_string):
     return json.loads(json_string)
 
 
+# # 專屬於job >> 寫到job class
+# def get_job_info_and_init(body):
+#     job_name = body["step_now"]
+#     job_info = body["steps"][job_name]
+#     job_info["start_time"] = get_now_time()
+#     return job_info
+
+
+# # 專屬於job >> 寫到job class
+# def update_job_info(job_info, results_output):
+#     job_info["end_time"] = get_now_time()
+#     job_info["result_output"] = {}
+#     config_outputs = parseString(job_info["config_output"])
+#     for output in config_outputs:
+#         out_put_name = output["name"]
+#         job_info["result_output"][out_put_name] = results_output[out_put_name]
+#     return job_info
+
+
+# 屬於一個queue的 ??
+# FIXME:????? 要寫在哪裡
+# 如果有客製化的時候要解析？ custom input??
 def replace_customize_variables(config, body):
     pattern = r"{{(.*?)}}"
     matches = re.findall(pattern, config)
@@ -58,3 +61,192 @@ def replace_customize_variables(config, body):
         # print(result_variable)
         config = config.replace("{{" + result + "}}", str(result_variable))
     return config
+
+
+# # 專屬於queue >> 寫到QueueObj class
+# def update_body(body, job_info):
+#     job_name = body["step_now"]
+#     body["steps"][job_name] = job_info
+#     return body
+
+
+class Job:
+    def __init__(self, job_info) -> None:
+        self.id = job_info.get("id")
+        self.name = job_info.get("name")
+        self.status = job_info.get("status", "waiting")  # init 的時候預設是waiting
+        self.sequence = job_info.get("sequence")
+
+        self.start_time = job_info.get("start_time")
+        self.end_time = job_info.get("end_time")
+
+        self.customer_input = job_info.get("customer_input")
+        self.config_output = job_info.get("config_output")
+
+        self.depends_job_instance_id = job_info.get("depends_job_instance_id")
+        self.result_output = job_info.get("result_output")
+
+        self.depends_job_id = job_info.get("depends_job_id")  ## FIXME: 待確認是否需要留存
+        self.function_name = job_info.get("function_name")
+
+    def create_job_instance(self, wf_instance_id, depends_job_instance_id, connDB):
+        self.depends_job_instance_id = depends_job_instance_id
+        sql = """
+            INSERT INTO 
+                jobs_instances
+                    (workflow_instance_id, name, status, sequence, customer_input, 
+                    config_output, depends_job_instance_id, function_name)
+                VALUES
+                    (%s, %s, %s, %s, %s, 
+                    %s, %s, %s)
+            """
+        params = (
+            wf_instance_id,
+            self.name,
+            self.status,
+            self.sequence,
+            self.customer_input,
+            self.config_output,
+            depends_job_instance_id,
+            self.function_name,
+        )
+        insertId = connDB.insert(sql, params)
+        self.id = insertId
+        return insertId
+
+    def update_start_time(self):
+        self.start_time = get_now_time()
+
+    def update_end_time(self):
+        self.end_time = get_now_time()
+
+    def update_result_output(self, results):
+        self.result_output = {}
+        outputs = parseString(self.template_output)
+        for output in outputs:
+            output_name = output["name"]
+            self.result_output[output_name] = results[output_name]
+
+
+class Workflow:
+    def __init__(self, workflow_Info):
+        self.id = workflow_Info.get("id")
+        self.name = workflow_Info.get("name")
+        self.trigger_type = workflow_Info.get("trigger_type")
+        self.next_execute_time = workflow_Info.get("next_execute_time")
+        self.schedule_interval = workflow_Info.get("schedule_interval")
+        self.trigger_interval_seconds = workflow_Info.get("trigger_interval_seconds")
+        self.status = workflow_Info.get("status", "queued")  # init的時候為queued
+        self.execution_time = workflow_Info.get("execution_time", get_now_time())  # init的時候為當下時間
+        self.wf_instance_id = workflow_Info.get("wf_instance_id")
+        self.schedule_interval = workflow_Info.get("schedule_interval")
+        self.manual_trigger = workflow_Info.get("manual_trigger", "f")  # default為"f"
+        self.end_time = workflow_Info.get("end_time")
+
+    def update_workflow_status(self, status):
+        self.status = status
+
+    def update_manual_trigger(self, ismanual_trigger):
+        self.manual_trigger = ismanual_trigger
+
+    def calculate_next_execute_time(self):
+        # 假設都是此 workflow trigger_type schedule 的情況下
+        if self.schedule_interval == "custom":
+            interval = timedelta(seconds=int(self.trigger_interval_seconds))
+        elif self.schedule_interval == "hourly":
+            interval = timedelta(hours=1)
+        elif self.schedule_interval == "daily":
+            interval = timedelta(days=1)
+        elif self.schedule_interval == "weekly":
+            interval = timedelta(days=7)
+        elif self.schedule_interval == "monthly":
+            interval = relativedelta(months=1)
+
+        next_time = self.next_execute_time + interval
+        next_time = next_time.strftime("%Y-%m-%d %H:%M:%S")
+        self.next_execute_time = next_time
+        return next_time
+
+    def update_db_workflow_next_execute_time(self, connDB):
+        sql = "UPDATE workflows SET next_execute_time = %s WHERE id = %s"
+        result = connDB.update(sql, (self.next_execute_time, (self.id)))
+        if result == 1:
+            print(f"Normal: workflow_id={self.id} next_execute更新成功")
+        else:
+            print(f"Error: workflow_id={self.id} next_execute更新未成功")
+
+    def create_wf_instance(self, connDB):
+        sql = """INSERT INTO workflows_instances
+                    (workflow_id, `status`, trigger_type, execution_time, manual_trigger, end_time)
+                VALUES
+                    (%s, %s, %s, %s, %s, %s)
+            """
+        params = (
+            self.id,
+            self.status,
+            self.trigger_type,
+            self.execution_time,
+            self.manual_trigger,
+            self.end_time,
+        )
+        insertId = connDB.insert(sql, params)
+        self.wf_instance_id = insertId
+
+    def get_jobs(self, connDB):
+        sql = """ SELECT
+                    jobs.id,
+                    jobs.name,
+                    jobs.sequence,
+                    jobs.depends_job_id,
+                    jobs.customer_input,
+                    `functions`.template_output as config_output,
+                    `functions`.name as function_name
+                FROM `jobs`
+                    INNER JOIN `functions` ON jobs.function_id = `functions`.id
+                WHERE workflow_id = %s
+                ORDER BY sequence
+            """
+        jobs_detail = connDB.queryall(sql, (self.id))
+        return jobs_detail
+
+
+class QueueObj:
+    def __init__(self, target_queue, queue_Info) -> None:
+        self.target_queue = queue_Info.get("target_queue", target_queue)
+        self.ready_execute_job = queue_Info.get("ready_execute_job", [])
+        self.workflow = queue_Info.get("workflow", {})
+        self.steps = queue_Info.get("steps", {})
+        self.step_now = queue_Info.get("step_now", None)
+
+    def init_workflow(self, workflow_instance):
+        self.workflow = workflow_instance
+
+    def add_step(self, job_instance):
+        job_name = job_instance.get("name")
+        self.ready_execute_job.append(job_name)
+        self.steps[job_name] = job_instance
+
+    def change_step(self, job_instance):
+        self.step_now = job_instance.get("name")
+
+    def put_to_sqs(self):
+        print(f"預計加入sqs的內容: {json.dumps(vars(self))}")
+
+        if os.environ["ENV"] == "TEST":
+            response = "SAM LOCAL TESTing"
+            return response
+
+        client = boto3.client("sqs", region_name=os.environ["AWS_REGION_NAME"])
+        try:
+            response = client.send_message(
+                QueueUrl=f'{os.environ["AWS_QUEUE_URL"]}/{self.target_queue}',
+                MessageBody=json.dumps(vars(self)),
+            )
+            print("put_to_sqs Response: ", response)
+        except ClientError:
+            response = ClientError
+        finally:
+            return response
+
+    def printQueue(self):
+        print("目前queue狀況", vars(self))
