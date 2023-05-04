6228cfdae05090ae1ed33c0e064f4a7ecc360fd1
tzutingspace <tzutingh2@gmail.com>
2023-05-05 00:11:33 +0800

    [Modify] aws_blog_scrape replace special characters

--- lambda/code/fn/aws_blog_scrape/aws_blog_scrape.py
+++ lambda/code/fn/aws_blog_scrape/aws_blog_scrape.py
@@ -57,6 +57,9 @@ def get_aws_blog(user_input_num):
             # print("Title: ", post.find("h2").text)
             try:
                 title = post.find("h2").get_text(strip=True)
+                title = title.replace("\u00A0", " ")
+                title = title.replace("\xa0", " ")
+                # title = re.sub(r"[^a-zA-Z0-9\s]+", "", title) ## 極端寫法
             except AttributeError as e:
                 print("抓取 title 失敗導致 get text 失敗")
                 MyErrorHandler.handle_error("AttributeError", f"@get_aws_blog function {e}")
@@ -74,59 +77,61 @@ def get_aws_blog(user_input_num):
             # section
             # print("Section:", post.find("section").text.strip())
 
-            try:
-                section = post.find("section").get_text(strip=True)
-                section = section.replace("\u00A0", " ")
-                section = section.replace("\xa0", " ")
-            except AttributeError as e:
-                print("抓取 section 失敗導致 get text 失敗")
-                MyErrorHandler.handle_error("AttributeError", f"@get_aws_blog function {e}")
-                return False
+            # 字數過多通知無意義, 取消 section
+            # try:
+            #     section = post.find("section").get_text(strip=True)
+            #     section = section.replace("\u00A0", " ")
+            #     section = section.replace("\xa0", " ")
+            # except AttributeError as e:
+            #     print("抓取 section 失敗導致 get text 失敗")
+            #     MyErrorHandler.handle_error("AttributeError", f"@get_aws_blog function {e}")
+            #     return False
 
             # 組成一個字串
-            result_obj = {"Date": post_date_str, "Title": title, "URL": url, "Section": section}
+            result_obj = {"Date": post_date_str, "Title": title, "URL": url}
             result_obj_list.append(result_obj)
 
     return result_obj_list
 
 
 def lambda_handler(event, context):
-    # 每個 function 都要做的事
-    body = json.loads(event["Records"][0]["body"])
-    print(f"來源內容{body}")
-    queue_obj = QueueObj(None, body)
-    current_job = Job(queue_obj.steps[queue_obj.step_now])
-    current_job.update_start_time()  # 更新開始時間
-    customer_input = current_job.parse_customer_input(queue_obj.steps)
+    for messages in event["Records"]:
+        # 每個 function 都要做的事
+        body = json.loads(messages["body"])
+        print(f"來源內容{body}")
+        queue_obj = QueueObj(None, body)
+        current_job = Job(queue_obj.steps[queue_obj.step_now])
+        current_job.update_start_time()  # 更新開始時間
+        customer_input = current_job.parse_customer_input(queue_obj.steps)
 
-    # aws_blog_scrape 獨特做的事
-    user_input_num = int(customer_input["interval"])
+        # aws_blog_scrape 獨特做的事
+        user_input_num = int(customer_input["interval"])
 
-    result_obj_list = get_aws_blog(user_input_num)
+        result_obj_list = get_aws_blog(user_input_num)
 
-    # get data 異常
-    if not result_obj_list:
-        job_status = "failed"
-        results_output = {}
-        for output in parseString(current_job.config_output):
-            results_output[output["name"]] = "Error"
-            # 此 function 回覆 list
+        # get data 異常
+        if not result_obj_list:
+            job_status = "failed"
+            results_output = {}
+            for output in parseString(current_job.config_output):
+                results_output[output["name"]] = "Error"
+                # 此 function 回覆 list
 
-    if len(result_obj_list) == 0:
-        job_status = "unfulfilled"
-        results_output = {}
-        for output in parseString(current_job.config_output):
-            results_output[output["name"]] = "undefined"
-    else:
-        job_status = "success"
-        results_output = {"result_list": result_obj_list}
+        if len(result_obj_list) == 0:
+            job_status = "unfulfilled"
+            results_output = {}
+            for output in parseString(current_job.config_output):
+                results_output[output["name"]] = "undefined"
+        else:
+            job_status = "success"
+            results_output = {"result_list": result_obj_list}
 
-    # 每個 function 都要做的事
-    current_job.update_job_status(job_status)
-    current_job.update_result_output(results_output)
-    current_job.update_end_time()
+        # 每個 function 都要做的事
+        current_job.update_job_status(job_status)
+        current_job.update_result_output(results_output)
+        current_job.update_end_time()
 
-    queue_obj.update_job_status(current_job)
-    queue_obj.put_to_sqs()
+        queue_obj.update_job_status(current_job)
+        queue_obj.put_to_sqs()
 
     return {"lambda msg": results_output}
